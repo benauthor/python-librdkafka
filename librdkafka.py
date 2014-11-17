36bba0cd0b8ba20809c92ba7050cb23864b18c6b
Yung-Chin Oei <yungchin@yungchin.nl>
2014-11-17 14:38:37 +0000

    Implement rdkafka metadata API
    Signed-off-by: Yung-Chin Oei <yungchin@yungchin.nl>

--- librdkafka.py
+++ librdkafka.py
@@ -13,7 +13,7 @@ _ffi.cdef(
     typedef ... rd_kafka_topic_t;
     typedef ... rd_kafka_conf_t;
     typedef ... rd_kafka_topic_conf_t;
-    typedef enum { ... } rd_kafka_resp_err_t;
+    typedef enum {RD_KAFKA_RESP_ERR_NO_ERROR, ...} rd_kafka_resp_err_t;
 
     const char *rd_kafka_err2str (rd_kafka_resp_err_t err);
     rd_kafka_resp_err_t rd_kafka_errno2err (int errnox);
@@ -54,6 +54,46 @@ _ffi.cdef(
                           void *payload, size_t len,
                           const void *key, size_t keylen,
                           void *msg_opaque);
+
+    typedef struct rd_kafka_metadata_broker {
+            int32_t     id;             /* Broker Id */
+            char       *host;           /* Broker hostname */
+            int         port;           /* Broker listening port */
+    } rd_kafka_metadata_broker_t;
+
+    typedef struct rd_kafka_metadata_partition {
+            int32_t     id;             /* Partition Id */
+            rd_kafka_resp_err_t err;    /* Partition error reported by broker */
+            int32_t     leader;         /* Leader broker */
+            int         replica_cnt;    /* Number of brokers in 'replicas' */
+            int32_t    *replicas;       /* Replica brokers */
+            int         isr_cnt;        /* Number of ISR brokers in 'isrs' */
+            int32_t    *isrs;           /* In-Sync-Replica brokers */
+    } rd_kafka_metadata_partition_t;
+
+    typedef struct rd_kafka_metadata_topic {
+            char       *topic;          /* Topic name */
+            int         partition_cnt;  /* Number of partitions in 'partitions' */
+            struct rd_kafka_metadata_partition *partitions; /* Partitions */
+            rd_kafka_resp_err_t err;    /* Topic error reported by broker */
+    } rd_kafka_metadata_topic_t;
+
+    typedef struct rd_kafka_metadata {
+            int         broker_cnt;     /* Number of brokers in 'brokers' */
+            struct rd_kafka_metadata_broker *brokers;  /* Brokers */
+            int         topic_cnt;      /* Number of topics in 'topics' */
+            struct rd_kafka_metadata_topic *topics;    /* Topics */
+            int32_t     orig_broker_id; /* Broker originating this metadata */
+            char       *orig_broker_name; /* Name of originating broker */
+    } rd_kafka_metadata_t;
+
+    rd_kafka_resp_err_t
+    rd_kafka_metadata (rd_kafka_t *rk, int all_topics,
+                       rd_kafka_topic_t *only_rkt,
+                       const struct rd_kafka_metadata **metadatap,
+                       int timeout_ms);
+    void rd_kafka_metadata_destroy (const struct rd_kafka_metadata *metadata);
+
     """)
 _lib = _ffi.verify("#include <librdkafka/rdkafka.h>", libraries=['rdkafka'])
 
@@ -63,11 +103,14 @@ def _mk_errstr():
     return _ffi.new("char []", 512)
 
 
+def _err2str(err):
+    return _ffi.string(_lib.rd_kafka_err2str(err))
+
+
 def _errno2str(errno=None):
     """ Look up string error message for errno """
-    if errno is None:
-        errno = _ffi.errno
-    return _ffi.string(_lib.rd_kafka_err2str(_lib.rd_kafka_errno2err(errno)))
+    errno = _ffi.errno if errno is None else errno
+    return _err2str(_lib.rd_kafka_errno2err(errno))
 
 
 class LibrdkafkaException(Exception):
@@ -164,3 +207,43 @@ class Topic(object):
                  msg_opaque)
         if rv:
             raise LibrdkafkaException(_errno2str())
+
+
+class Metadata(object):
+    def __init__(self, kafka_handle, topic=None, timeout_ms=1000):
+        self.kafka_handle = kafka_handle
+        self.topic = topic
+        self.all_topics = 0 # TODO expose this option usefully?
+        self.refresh(timeout_ms)
+
+    def refresh(self, timeout_ms=1000):
+        topic = _ffi.NULL if self.topic is None else self.topic.cdata
+        meta_dp = _ffi.new("const rd_kafka_metadata_t **")
+        err = _lib.rd_kafka_metadata(
+                  self.kafka_handle.cdata, self.all_topics,
+                  topic, meta_dp, timeout_ms)
+        if err != _lib.RD_KAFKA_RESP_ERR_NO_ERROR:
+            raise LibrdkafkaException(_err2str(err))
+
+        self.dict = d = {}
+        meta = meta_dp[0]
+        d['brokers'] = {}
+        for i in range(meta.broker_cnt):
+            b = meta.brokers[i]
+            d['brokers'][b.id] = dict(host=_ffi.string(b.host), port=b.port)
+        d['topics'] = {}
+        for i in range(meta.topic_cnt):
+            t = meta.topics[i]
+            d['topics'][_ffi.string(t.topic)] = topic_d = {}
+            topic_d['partitions'] = {}
+            for j in range(t.partition_cnt):
+                p = t.partitions[j]
+                topic_d['partitions'][p.id] = dict(
+                        err=_err2str(p.err), leader=p.leader,
+                        replicas=[p.replicas[r] for r in range(p.replica_cnt)],
+                        isrs=[p.isrs[r] for r in range(p.isr_cnt)])
+            topic_d['err'] = _err2str(t.err)
+        d['orig_broker_id'] = meta.orig_broker_id
+        d['orig_broker_name'] = _ffi.string(meta.orig_broker_name)
+
+        _lib.rd_kafka_metadata_destroy(meta_dp[0])
