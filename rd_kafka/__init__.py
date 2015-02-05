benauthor/python-librdkafka

commit ac1ea0ca34d907214784e98d6d82157a6a01abcd

remove TopicConfig class
Similar to what the parent commit did for Config.

Signed-off-by: Yung-Chin Oei <yungchin@yungchin.nl>

rd_kafka/__init__.py
@@ -17,10 +17,19 @@ class BaseTopic(object):
     def __init__(self, name, kafka_handle, topic_config_dict):
         self.kafka_handle = kafka_handle
 
-        cfg = _config_handles.TopicConfig(topic_config_dict)
+        conf = _lib.rd_kafka_topic_conf_new()
+        for name, value in topic_config_dict.items():
+            if name == "partitioner":
+                _config_handles.topic_conf_set_partitioner_cb(conf, value)
+            else:
+                errstr = _mk_errstr()
+                res = _lib.rd_kafka_topic_conf_set(
+                          conf, name, value, errstr, len(errstr))
+                if res != _lib.RD_KAFKA_CONF_OK:
+                    raise LibrdkafkaException(_ffi.string(errstr))
+
         self.cdata = _lib.rd_kafka_topic_new(
-                         self.kafka_handle.cdata, name, cfg.cdata)
-        cfg.cdata = None # prevent double-free in cfg.__del__()
+                         self.kafka_handle.cdata, name, conf)
         if self.cdata == _ffi.NULL:
             raise LibrdkafkaException(_errno2str())
 

rd_kafka/_config_handles.py
@@ -1,13 +1,9 @@
 from .headers import ffi as _ffi, lib as _lib
 from .message import Message
-from .utils import _mk_errstr, _voidp2bytes
+from .utils import _voidp2bytes
 
 
 callback_funcs = [] # TODO need a better place to keep our cffi callbacks alive
-
-
-class ConfigException(Exception):
-    pass
 
 
 def conf_set_dr_msg_cb(conf_handle, callback_func):
@@ -37,54 +33,29 @@ def conf_set_dr_msg_cb(conf_handle, callback_func):
     _lib.rd_kafka_conf_set_dr_msg_cb(conf_handle, func)
 
 
-class TopicConfig(object):
-    def __init__(self, topic_config_dict={}):
-        self.cdata = _lib.rd_kafka_topic_conf_new()
-        for k, v in topic_config_dict.items():
-            self.set(k, v)
+def topic_conf_set_partitioner_cb(topic_conf_handle, callback_func):
+    """
+    Python callback to partition messages
 
-    def __copy__(self):
-        raise NotImplementedError
+    Set a python callback_func with signature
+        'key_string, partitions_available_list => item'
+    where item is selected from partition_list, or None if the function
+    couldn't decide a partition.
+    """
+    @_ffi.callback("int32_t (const rd_kafka_topic_t *, const void *,"
+                   "         size_t, int32_t, void *, void *)")
+    def func(topic, key, key_len, partition_cnt, t_opaque, m_opaque):
+        key = _voidp2bytes(key, key_len)[:]
+        partition_list = range(partition_cnt)
+        while partition_list:
+            p = callback_func(key, partition_list)
+            if (p is None or
+                    _lib.rd_kafka_topic_partition_available(topic, p)):
+                break
+            else:
+                partition_list.remove(p)
+                p = None
+        return _lib.RD_KAFKA_PARTITION_UA if p is None else p
 
-    def __deepcopy__(self, memo):
-        raise NotImplementedError
-
-    def __del__(self):
-        if self.cdata is not None:
-            _lib.rd_kafka_topic_conf_destroy(self.cdata)
-
-    def set(self, name, value):
-        errstr = _mk_errstr()
-        if name == "partitioner":
-            return self.set_partitioner_cb(value)
-        res = _lib.rd_kafka_topic_conf_set(
-                  self.cdata, name, value, errstr, len(errstr))
-        if res != _lib.RD_KAFKA_CONF_OK:
-            raise ConfigException(_ffi.string(errstr))
-
-    def set_partitioner_cb(self, callback_func):
-        """
-        Python callback to partition messages
-
-        Set a python callback_func with signature
-            'key_string, partitions_available_list => item'
-        where item is selected from partition_list, or None if the function
-        couldn't decide a partition.
-        """
-        @_ffi.callback("int32_t (const rd_kafka_topic_t *, const void *,"
-                       "         size_t, int32_t, void *, void *)")
-        def func(topic, key, key_len, partition_cnt, t_opaque, m_opaque):
-            key = _voidp2bytes(key, key_len)[:]
-            partition_list = range(partition_cnt)
-            while partition_list:
-                p = callback_func(key, partition_list)
-                if (p is None or
-                        _lib.rd_kafka_topic_partition_available(topic, p)):
-                    break
-                else:
-                    partition_list.remove(p)
-                    p = None
-            return _lib.RD_KAFKA_PARTITION_UA if p is None else p
-
-        callback_funcs.append(func) # prevent garbage-collection of func
-        _lib.rd_kafka_topic_conf_set_partitioner_cb(self.cdata, func)
+    callback_funcs.append(func) # prevent garbage-collection of func
+    _lib.rd_kafka_topic_conf_set_partitioner_cb(topic_conf_handle, func)

commit dfe01acc22a3924c4ba670945e79b555968dd0a6

BaseTopic: fix bug that overwrote topic name
This was introduced with ac1ea0ca34d907214784e98d6d82157a6a01abcd
through careless refactoring, but eluded me so far, because the test I
wrote trying to demonstrate the problem was incomplete: it didn't use a
custom topic_config, thereby skipping the offending code block.
Clearly, we need coverage tools etc soon.

Resolves #2.

Signed-off-by: Yung-Chin Oei <yungchin@yungchin.nl>

rd_kafka/__init__.py
@@ -19,14 +19,14 @@ class BaseTopic(object):
 
         self.conf_callbacks = [] # keeps callback handles alive
         conf = _lib.rd_kafka_topic_conf_new()
-        for name, value in topic_config_dict.items():
-            if name == "partitioner":
+        for k, v in topic_config_dict.items():
+            if k == "partitioner":
                 self.conf_callbacks.append(
-                    _config_handles.topic_conf_set_partitioner_cb(conf, value))
+                    _config_handles.topic_conf_set_partitioner_cb(conf, v))
             else:
                 errstr = _mk_errstr()
                 res = _lib.rd_kafka_topic_conf_set(
-                          conf, name, value, errstr, len(errstr))
+                            conf, k, v, errstr, len(errstr))
                 if res != _lib.RD_KAFKA_CONF_OK:
                     raise LibrdkafkaException(_ffi.string(errstr))
 

tests.py
@@ -188,7 +188,8 @@ class ProduceConsumeTestCase(unittest.TestCase):
 
         config["dr_msg_cb"] = dr_msg_cb
         producer = Producer(config)
-        p_topic = producer.open_topic("ProduceConsumeTestCase")
+        p_topic = producer.open_topic("ProduceConsumeTestCase",
+                                      {"request.required.acks": "-1"})
 
         consumer = Consumer(config)
         reader = consumer.new_queue()